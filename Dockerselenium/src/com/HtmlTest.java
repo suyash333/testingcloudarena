3e429dec49ca6ba159871f36708b6500bcc93740
333.suyash@gmail.com <ramram@123>
2017-10-29 23:12:33 +0530

    edited programs

--- Dockerselenium/src/com/HtmlTest.java
+++ Dockerselenium/src/com/HtmlTest.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
-public class HtmlTest {
+class HtmlTest {
 
 	/**
 	 * @param args
